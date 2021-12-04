a982840bb302bb7bebac266b8afec0f45038706c
David Volm <david@volminator.com>
2021-12-04 14:46:24 -0600

    Comment out for production version

--- backend/autoload.php
+++ backend/autoload.php
@@ -1,4 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
-header('Access-Control-Allow-Credentials: true');
+// uncomment for testing on local machine
+// header('Access-Control-Allow-Origin: http://localhost:3000');
+// header('Access-Control-Allow-Credentials: true');
 ?>
